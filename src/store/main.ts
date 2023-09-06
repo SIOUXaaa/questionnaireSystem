756edadeecef2f1225d37ff67168b54439e37244
SIOUXaaa <3011057139@qq.com>
2023-09-06 22:31:36 +0800

    init

---
+++ src/store/main.ts
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const useMain = defineStore('main', {
+    state: () => {
+        return {
+            
+        }
+    },
+    // 计算属性
+    getters: {
+
+    },
+    actions: {
+        
+    }
+
+})
